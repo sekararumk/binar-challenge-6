092d35f6b6d752a838c61a1cb6d0fb2a0698063b
sekararumk <sekararumk21@gmail.com>
2022-05-13 23:26:46 +0700

    update challenge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useEffect} from 'react'
+import React, { useEffect, useState} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import jwtDecode from "jwt-decode"
 import axios from "axios"
 import { useDispatch } from "react-redux"
-
+import Async from "async"
 import ProtectedRoute from './Component/HOC/ProtectedRoute'
 import UnprotectedRoute from './Component/HOC/UnprotectedRoute'
 import Login from './Component/Page/Login'
@@ -14,10 +14,25 @@ import AdminPage from './Component/Page/AdminPage'
 import Logout from './Component/Page/Logout'
 import userSlice from './store/user'
 
+// const getUser = async () => {
+//   try {
+//     const token = localStorage.getItem('carAccessToken')
+//     const userData = jwtDecode(token)
+//     const res = await axios.get(`http://localhost:4000/users/${userData.sub}`)
+//     return {
+//       user: res.data
+//     } 
+//   } catch {
+//     return {
+//       user: null
+//     }
+//   }
+// }
+
 const App = () => {
 
   const dispatch = useDispatch()
-
+  // const [setLogin] = useState([])
   useEffect( () => {
     try {
       const token = localStorage.getItem('carAccessToken')
@@ -29,32 +44,53 @@ const App = () => {
     } catch {}
     
   }, [])
-
   return (
-    <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          {/* ALL */}
-          <Route index element={<Home />} />
-          <Route path="logout" element={<Logout />} />
+    // <Async promiseFn={getUser}>
+    // {( {data, error, isPending} ) => {
+    //   if(isPending) {
+    //     return (
+    //       <h2>Loading...</h2>
+    //     )
+    //   }
+    //   if(error) {
+    //     return (
+    //       <h2>Error</h2>
+    //     )
+    //   }
+    //   if(data){
+    //     if ( data.user !== null ){
+    //       dispatch(userSlice.actions.addUser( {userData: data.user} ))
+    //     }
+    //     return (
+          <>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/">
+                  {/* ALL */}
+                  <Route index element={<Home />} />
+                  <Route path="logout" element={<Logout />} />
 
-          {/* PUBLIC ONLY */}
-          <Route path="/" element={<UnprotectedRoute />} >
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Route>
-          
-          {/* PROTECTED */}
-          <Route path="/" element={<ProtectedRoute />} >
-            <Route path="admin-page" element={<AdminPage />} />
-            <Route path="user-page" element={<UserPage />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
-  );
+                  {/* PUBLIC ONLY */}
+                  <Route path="/" element={<UnprotectedRoute />} >
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                  </Route>
+                  
+                  {/* PROTECTED */}
+                  <Route path="/" element={<ProtectedRoute />} >
+                    <Route path="admin-page" element={<AdminPage />} />
+                    <Route path="user-page" element={<UserPage />} />
+                  </Route>
+                </Route>
+              </Routes>
+              {/* <Login setLogin={setLogin} /> */}
+            </BrowserRouter>
+          </>
+        )
+    //     }
+    //   }}
+  //   // </Async>
+  // );
 }
 
 export default App;
